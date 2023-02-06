3a8d06c735610f1ffc49c072c64c5a12ff7e6e57
hadisyekta <hadis.yekta2@gmail.com>
2023-02-06 02:58:50 -0500

    create list and details templates

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import notes.views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+    path('', notes.views.noteList, name='notes-list'),
+    path('notes/<int:id>/', notes.views.noteDetails, name='note-details')
 ]
--- app/notes/views.py
+++ app/notes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+from notes.models import Notes
+
+def noteList(request):
+    context = {
+        'notes': Notes.objects.all(),
+    }
+    return render(request, 'notes/notes_list.html', context)
+
+def noteDetails(request, id):
+    context = {
+        'note': Notes.objects.get(id=id),
+    }
+    return render(request, 'notes/note.html', context)
