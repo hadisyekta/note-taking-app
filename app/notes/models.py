238771e99e28a8bf4b5af8275f2a9c9659fcbfb2
hadisyekta <hadis.yekta2@gmail.com>
2023-02-06 16:14:50 -0500

    update tags in a note

--- app/notes/models.py
+++ app/notes/models.py
@@ -19,8 +19,8 @@ class Notes(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     tags = models.ManyToManyField(Tags, blank=True, related_name="tags")
-    created = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
     private = models.BooleanField('Private', default=True)
 
--- app/notes/serializers.py
+++ app/notes/serializers.py
@@ -14,19 +14,25 @@ class TagsSerializer(serializers.ModelSerializer):
 class NotesSerializer(serializers.ModelSerializer):
     title = serializers.CharField(min_length=2, max_length=100)
     tags = TagsSerializer(read_only=True, many=True)
+    tags_id = serializers.PrimaryKeyRelatedField(queryset=Tags.objects.all(), write_only=True,many=True)
     body = serializers.CharField(min_length=2, max_length=500)
     private = serializers.BooleanField()
     author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
         model = Notes
-        fields = '__all__'
-        # exclude = ['author', ]
+        fields = ('id', 'author', 'title', 'body', 'tags', 'tags_id', 'private', 'created_at','updated_at',)
+        read_only_fields = ('id', 'author', 'tags_id', 'created_at','updated_at',)
 
     def create(self, validated_data):
         request = self.context.get('request', None)
         if request:
             validated_data['author'] = request.user
         print(validated_data)
+        tags = validated_data.pop('tags_id')
+        note = Notes.objects.create(**validated_data)
+        for tag in tags:
+            note.tags.add(tag)
+        # or : note.tags.add(*tags)
 
-        return Notes.objects.create(**validated_data)
+        return note
